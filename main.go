284fc67cc7ce79f49ca3b6adfe1e4c92195c8c9e
Gennady Badinov <gennady.badinov@gmail.com>
2023-02-22 18:25:15 +0300

    Clean up the code

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import (
 	"strings"
 )
 
+type problem struct {
+	q string
+	a string
+}
+
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'problem,answer'")
 	flag.Parse()
@@ -16,6 +21,7 @@ func main() {
 	if err != nil {
 		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
 	}
+	defer file.Close()
 
 	r := csv.NewReader(file)
 	records, err := r.ReadAll()
@@ -24,11 +30,26 @@ func main() {
 	}
 	problems := parseLines(records)
 
-	var score int
+	score := playQuiz(problems)
+	fmt.Printf("You scored %d out of %d.\n", score, len(problems))
+}
 
+func parseLines(lines [][]string) []problem {
+	problems := make([]problem, len(lines))
+	for i, line := range lines {
+		problems[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return problems
+}
+
+func playQuiz(problems []problem) int {
+	score := 0
 	for i, p := range problems {
 		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
-		var answer string
+		answer := ""
 		fmt.Scanf("%s \n", &answer)
 		if answer == p.a {
 			score++
@@ -37,28 +58,10 @@ func main() {
 			fmt.Println("Incorrect!")
 		}
 	}
-
-	fmt.Printf("You scored %d out of %d.", score, len(problems))
-
+	return score
 }
 
 func exit(msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
-}
-
-type problem struct {
-	q string
-	a string
-}
-
-func parseLines(lines [][]string) []problem {
-	ret := make([]problem, len(lines))
-	for i, line := range lines {
-		ret[i] = problem{
-			q: line[0],
-			a: strings.TrimSpace(line[1]),
-		}
-	}
-	return ret
 }
