1142cce831a236b0c0dfd7262c413a088c0db550
Gennady Badinov <gennady.badinov@gmail.com>
2023-02-22 18:13:55 +0300

    Handle users answers

--- main.go
+++ main.go
@@ -23,9 +23,25 @@ func main() {
 		exit("Failed to parse the provided CSV file.")
 	}
 	problems := parseLines(records)
-	fmt.Println(problems)
+
+	var score int
+
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s \n", &answer)
+		if answer == p.a {
+			score++
+			fmt.Println("Correct!")
+		} else {
+			fmt.Println("Incorrect!")
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.", score, len(problems))
 
 }
+
 func exit(msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
