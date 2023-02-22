a0cae1c2a8eac5421baa5b5265de24eaad8e77a9
Gennady Badinov <gennady.badinov@gmail.com>
2023-02-22 17:19:23 +0300

    Add CLI flag to open a CSV file

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'problem,answer'")
+	flag.Parse()
+
+	_, err := os.Open(*csvFilename)
+	if err != nil {
+		fmt.Printf("Failed to open the CSV file: %s\n", *csvFilename)
+		os.Exit(1)
+	}
+
+}
